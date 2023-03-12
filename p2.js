1001380a6b160f04c463fa1ce5f187aedc65c102
Basel Allam <97627949+BOLT-7@users.noreply.github.com>
2023-03-12 15:22:12 -0300

    check image psuedo function

---
+++ p2.js
@@ -0,0 +1,16 @@
+
+/**
+ * A function to check if the bear was dropped in the correct position
+ * 
+ * @param {*} ev event object loaded with drop event info
+ * @author Basel
+ */
+function checkImage(ev) {
+    ev.preventDefault();
+
+    if (ev.target.id == 1 /*how Bhaumik decided to implement the words array*/ ) {
+        /* JQUERY to insert the correct display into the html*/
+    } else {
+        /* JQUERY to insert the incorrect display into the html*/
+    }
+}
