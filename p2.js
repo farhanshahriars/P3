b10bf9440d31fa0feef5c29fcf4e5b9d514b0cc7
Basel Allam <97627949+BOLT-7@users.noreply.github.com>
2023-03-14 21:31:33 -0300

    Update p2.js

--- p2.js
+++ p2.js
@@ -1,16 +1,38 @@
-
 /**
- * A function to check if the bear was dropped in the correct position
+ * The purpose of this file is to provide functionality for the grid and the entire webpage
  * 
- * @param {*} ev event object loaded with drop event info
  * @author Basel
  */
-function checkImage(ev) {
-    ev.preventDefault();
 
-    if (ev.target.id == 1 /*how Bhaumik decided to implement the words array*/ ) {
-        /* JQUERY to insert the correct display into the html*/
-    } else {
-        /* JQUERY to insert the incorrect display into the html*/
-    }
+/*Global variables*/
+
+//An array to hold the audio
+const audioList = ["./audios/ltu.wav", "./audios/kil.wav", "./audios/aqq.wav"];
+
+//An array to hold the images
+const imageList = ["./images/ltuText.jpg", "./images/kilText.jpg", "./images/aqqText.jpg"]
+
+/**
+ * A function to play the audio for the displayed word (still need to link the audio and word together)
+ * 
+ * @author Basel
+ */
+function playAudio() {
+    //var audio = new Audio('./audios/ltu.wav');
+    var audio = new Audio();
+    audio.src = audioList[Math.floor(Math.random() * 2)];
+    audio.play();
+}
+
+/**
+ * A function to load the starting word (still need to link the audio and word together)
+ * 
+ * @author Basel
+ */
+function loadStartQuestion() {
+    var loadImage = imageList[Math.floor(Math.random() * 2)];
+    console.log(loadImage);
+    $(".question").append(
+        "<img src= " + loadImage + ' alt="">'
+      );
 }
