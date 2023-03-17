7be41be4390ff87d1f959d4ec97ec156abdaba1a
Basel Allam <97627949+BOLT-7@users.noreply.github.com>
2023-03-17 18:59:55 -0300

    Update p2.js

--- p2.js
+++ p2.js
@@ -7,31 +7,32 @@
 /*Global variables*/
 
 //An array to hold the audio
-const audioList = ["./audios/ltu.wav", "./audios/kil.wav", "./audios/aqq.wav"];
+const audioList = ["./audios/aqq.wav", "./audios/eliey.wav", "./audios/kesalk.wav", "./audios/kil.wav", "./audios/ltu.wav", "./audios/mijisi.wav", "./audios/nin.wav", "./audios/teluisi.wav", "./audios/wiktm.wav"];
 
 //An array to hold the images
-const imageList = ["./images/ltuText.jpg", "./images/kilText.jpg", "./images/aqqText.jpg"]
+const imageList = ["./images/aqqText.jpg", "./images/elieyText.jpg", "./images/kesalkText.jpg", "./images/kilText.jpg", "./images/ltuText.jpg", "./images/mijisiText.jpg", "./images/ninText.jpg", "./images/teluisiText.jpg", "./images/wiktmText.jpg"];
+
+//A value to link the audio button with the word on screen so it plays the correct audio
+let startQuestion = Math.floor(Math.random() * 3);
 
 /**
- * A function to play the audio for the displayed word (still need to link the audio and word together)
+ * A function to play the audio for the displayed word 
  * 
  * @author Basel
  */
 function playAudio() {
-    //var audio = new Audio('./audios/ltu.wav');
     var audio = new Audio();
-    audio.src = audioList[Math.floor(Math.random() * 2)];
+    audio.src = audioList[startQuestion];
     audio.play();
 }
 
 /**
- * A function to load the starting word (still need to link the audio and word together)
+ * A function to load the starting word 
  * 
  * @author Basel
  */
 function loadStartQuestion() {
-    var loadImage = imageList[Math.floor(Math.random() * 2)];
-    console.log(loadImage);
+    var loadImage = imageList[startQuestion];
     $(".question").append(
         "<img src= " + loadImage + ' alt="">'
       );
